19f3b96f93e10f111998bdac1917b0aba55317cf
huayu-ouyang <ouyang.huayu@gmail.com>
2021-01-27 21:35:50 -0800

    first commit

---
+++ my-app/src/fire.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDijFz9Wlth9-Xam9EAfNfb01f64Zfhpu4",
+    authDomain: "test-react-firebase-9196d.firebaseapp.com",
+    databaseURL: "https://test-react-firebase-9196d-default-rtdb.firebaseio.com",
+    projectId: "test-react-firebase-9196d",
+    storageBucket: "test-react-firebase-9196d.appspot.com",
+    messagingSenderId: "367590413458",
+    appId: "1:367590413458:web:9cbff6bacdf3e0799e424b"
+  };
+  // Initialize Firebase
+  var fire = firebase.initializeApp(firebaseConfig);
+export default fire;
+
+
+
